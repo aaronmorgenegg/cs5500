44b16a82f1d94c687440512abf1806445ddcb920
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-08-30 15:09:56 -0600

    Basic message passing between processes

--- assn1/main.cpp
+++ assn1/main.cpp
@@ -3,7 +3,6 @@
 
 int main(int argc, char** argv) {
 	// initialize MPI
-	// MPI_Init(&argc, &argv);
 	MPI_Init(&argc, &argv);
 
 	// stores number of processes in world_size
@@ -14,8 +13,27 @@ int main(int argc, char** argv) {
 	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-	// Hello World!
-	std::cout << "Hello world! I am  " <<  world_rank << " of " << world_size << std::endl;
+	int data = world_rank;
+	MPI_Send(
+		&data, // data to send
+		1, // count, or number of things passed
+		MPI_INT, // datatype
+		(world_rank+1)%world_size, // destination
+		0, // tag of message
+		MPI_COMM_WORLD // MPI communicator
+	);
+
+	MPI_Recv(
+		&data, // data to recieve
+		1, // count, or number of things passed
+	    	MPI_INT, // datatype
+		MPI_ANY_SOURCE, // source
+		MPI_ANY_TAG, // tag of message
+		MPI_COMM_WORLD, // MPI communicator
+		MPI_STATUS_IGNORE // MPI status
+	);
+
+	std::cout << "I am " << world_rank << " of " << world_size << ", recieving a message from number " << data << std::endl;
 
 	// Finalize the MPI environment.
 	MPI_Finalize();
