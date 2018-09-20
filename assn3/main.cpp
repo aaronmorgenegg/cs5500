f082e008994b3ed1f9038bcc2d48b4a05a5b2227
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-19 19:39:42 -0600

    Added sorting, basic master/slave functionality

--- assn3/main.cpp
+++ assn3/main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <unistd.h>
+#include <algorithm>
 
 const int MAX_INT_SIZE = 1000;
 const int LIST_SIZE = 100;
@@ -15,14 +16,47 @@ int * GetUnsortedList(int n){
 	return list;
 }
 
-void MasterRoutine(){
-	// Code to run if you're the big boss man
-	int * unsorted_list = GetUnsortedList(LIST_SIZE);
+void PrintList(int * list){
+	for(int i = 0; i < LIST_SIZE; i++){
+		std::cout<<list[i]<<",";
+	}
+	std::cout << std::endl;
 }
 
-void SlaveRoutine(){
+int * SortList(int * unsorted_list){
+	std::sort(unsorted_list, unsorted_list+LIST_SIZE);
+	return unsorted_list;
+}
+
+int * MergeLists(int ** split_lists){
+	return new int;
+}
+
+void MasterRoutine(int world_size){
+	// Code to run if you're the big boss man
+	int * unsorted_list = GetUnsortedList(LIST_SIZE);
+	std::cout << "Sorting the following list: " << std::endl;
+	PrintList(unsorted_list);
+	for(int i = 1; i < world_size; i++){
+		MPI_Send(&unsorted_list, LIST_SIZE/(world_size-1), MPI_INT, i, 0, MPI_COMM_WORLD);
+		unsorted_list += LIST_SIZE/(world_size-1);
+	}
+	int ** split_lists = new int *;
+	// Wait to receive the split lists
+	for(int i = 1; i < world_size; i++){
+		MPI_Recv(split_lists[i], LIST_SIZE/(world_size-1), MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	}
+	int * sorted_list = MergeLists(split_lists);
+	std::cout << "Sorted list: " << std::endl;
+	PrintList(sorted_list);
+}
+
+void SlaveRoutine(int world_size){
 	// Code to run if you're not the big boss man
-	
+	int * data;
+        MPI_Recv(data, LIST_SIZE/(world_size-1), MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	SortList(data);
+	MPI_Send(data, LIST_SIZE/(world_size-1), MPI_INT, 0, 0, MPI_COMM_WORLD);
 }
 
 int main(int argc, char** argv) {
@@ -41,11 +75,10 @@ int main(int argc, char** argv) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
 	if(world_rank == 0){
-		MasterRoutine();
+		MasterRoutine(world_size);
 	} else {
-		SlaveRoutine();
+		SlaveRoutine(world_size);
 	}
-
 	// Finalize the MPI environment.
 	MPI_Finalize();
 	return 0;
