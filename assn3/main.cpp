74817cffb62579c8374873e2fccc79282da80548
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-20 15:10:51 -0600

    Refactoring to use the MPI_Scatter function

--- assn3/main.cpp
+++ assn3/main.cpp
@@ -23,8 +23,8 @@ void PrintList(int * list){
 	std::cout << std::endl;
 }
 
-int * SortList(int * unsorted_list){
-	std::sort(unsorted_list, unsorted_list+LIST_SIZE);
+int * SortList(int * unsorted_list, int list_length){
+	std::sort(unsorted_list, unsorted_list+list_length);
 	return unsorted_list;
 }
 
@@ -44,36 +44,11 @@ int * MergeLists(int ** split_lists, int world_size){
 	return merged_list;
 }
 
-void MasterRoutine(int world_size){
-	// Code to run if you're the big boss man
+int * CreateAndPrintList(int world_size){
 	int * unsorted_list = GetUnsortedList(LIST_SIZE);
 	std::cout << "Sorting the following list: " << std::endl;
 	PrintList(unsorted_list);
-	for(int i = 1; i < world_size; i++){
-		MPI_Send(&unsorted_list, LIST_SIZE/(world_size-1), MPI_INT, i, 0, MPI_COMM_WORLD);
-		unsorted_list += LIST_SIZE/(world_size-1);
-	}
-	int ** split_lists = new int*[world_size-1];
-	for(int i = 0; i < world_size-1; i++){
-		split_lists[i] = new int[LIST_SIZE/(world_size-1)];
-	}
-	// Wait to receive the split lists
-	for(int i = 0; i < world_size-1; i++){
-		MPI_Recv(split_lists[i], LIST_SIZE/(world_size-1), MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	}
-	std::cout<<"check"<<std::endl;
-	PrintList(split_lists[0]);
-	int * sorted_list = MergeLists(split_lists, world_size);
-	std::cout << "Sorted list: " << std::endl;
-	PrintList(sorted_list);
-}
-
-void SlaveRoutine(int world_size){
-	// Code to run if you're not the big boss man
-	int * data;
-        MPI_Recv(data, LIST_SIZE/(world_size-1), MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	SortList(data);
-	MPI_Send(data, LIST_SIZE/(world_size-1), MPI_INT, 0, 0, MPI_COMM_WORLD);
+	return unsorted_list;
 }
 
 int main(int argc, char** argv) {
@@ -91,11 +66,19 @@ int main(int argc, char** argv) {
 	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
+	int sub_number = LIST_SIZE/(world_size);
+
+	int * unsorted_list;
 	if(world_rank == 0){
-		MasterRoutine(world_size);
-	} else {
-		SlaveRoutine(world_size);
+		unsorted_list = CreateAndPrintList(world_size);
 	}
+	int * sub_list = new int[sub_number];
+	MPI_Scatter(unsorted_list, sub_number, MPI_INT, sub_list, sub_number, MPI_INT, 0, MPI_COMM_WORLD);
+
+	SortList(sub_list, sub_number);
+
+	PrintList(sub_list);
+
 	// Finalize the MPI environment.
 	MPI_Finalize();
 	return 0;
