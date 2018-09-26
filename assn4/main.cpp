16f0f1cd33ddac372cfb29048c0b8289f03fd635
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-26 15:02:57 -0600

    Added list creation and printing functions

--- assn4/main.cpp
+++ assn4/main.cpp
@@ -5,15 +5,34 @@
 #include <unistd.h>
 #include <algorithm>
 
+int * getBitonicList(int size){ // Create a bitonic list of given size
+	// CAUTION : designed to work with size=power of 2
+	int * list = new int[size];
+	int value = size/2;
+	for(int i = 0; i < size; i++){
+		list[i] = value;
+		if(value == 1){
+			value = size;
+		} else{
+			value--;
+		}
+	}
+	return list;
+}
+
+int * printList(int * list, int size){
+	for(int i=0;i<size;++i){
+		std::cout<<i<<":";
+		std::cout<<list[i]<<" ";
+	}
+	std::cout<<std::endl;
+}
+
 void print1per(int data, int rank, int size){ // Print 1 per function from class
 	int *dArray = new int [size];
-	MPI_Gather(&data,1,MPI_INT,dArray,1,MPI_INT,0,MCW);
+	MPI_Gather(&data,1,MPI_INT,dArray,1,MPI_INT,0,MPI_COMM_WORLD);
 	if(rank==0){
-		for(int i=0;i<size;++i){
-			cout<<i<<":";
-			cout<<dArray[i]<<" ";
-		}
-		cout<<endl;
+		printList(darray, size);
 	}
 	return;
 }
@@ -21,35 +40,30 @@ void print1per(int data, int rank, int size){ // Print 1 per function from class
 int cube(int i, int sendData){ // Cube function from class
 	int rank;
 	int size;
-	MPI_Comm_rank(MCW, &rank); 
-	MPI_Comm_size(MCW, &size); 
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+	MPI_Comm_size(MPI_COMM_WORLD, &size); 
 	int dest;
 	int recvData;
 	int mask=1;
 	mask = mask << i;
 	dest = rank ^ mask;
-	MPI_Send(&sendData,1,MPI_INT,dest,0,MCW);
-	MPI_Recv(&recvData,1,MPI_INT,dest,0,MCW,MPI_STATUS_IGNORE);
+	MPI_Send(&sendData,1,MPI_INT,dest,0,MPI_COMM_WORLD);
+	MPI_Recv(&recvData,1,MPI_INT,dest,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 
 	return recvData;
 }
 
 int main(int argc, char** argv) {
-	// initialize MPI
 	MPI_Init(&argc, &argv);
-
-	// Initialize rng
 	srand(time(NULL));
-
-	// stores number of processes in world_size
 	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-	// Get the rank of this process
 	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-	// Finalize the MPI environment.
+	int * list = getBitonicList(8);
+	printList(list, 8);
+
 	MPI_Finalize();
 	return 0;
 }
