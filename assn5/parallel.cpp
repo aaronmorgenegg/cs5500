32f4f11563fa5faa11973a300f153ef02c0c3d25
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-10-02 20:05:13 -0600

    Made fixes to parallel routine to prevent segfault

--- assn5/parallel.cpp
+++ assn5/parallel.cpp
@@ -9,7 +9,7 @@
 #include <string>
 
 const int MAX_ITERATION = 1000;
-const int RESOLUTION = 512;
+const int RESOLUTION = 2048;
 const bool INVERT_COLORS = false;
 const std::string OUTPUT_FILE = "parallel_mandelbrot.ppm";
 
@@ -40,11 +40,20 @@ Color getColor(int iteration){
 	return Color(r,g,b);
 }
 
+std::ofstream setupFile(){
+	std::ofstream mandelbrot_file(OUTPUT_FILE);
+	mandelbrot_file << "P3" << std::endl;
+	mandelbrot_file << RESOLUTION << " " << RESOLUTION << std::endl;
+	mandelbrot_file << "255" << std::endl;
+	return mandelbrot_file;
+}
+
 void writeToFile(std::string message, std::ofstream &mandelbrot_file){
 	mandelbrot_file << message;	
 }
 
-void plotImage(Color plot[][RESOLUTION], std::ofstream &mandelbrot_file){
+void plotImage(Color** plot){
+	std::ofstream mandelbrot_file = setupFile();
 	for(int i = 0; i < RESOLUTION; i++){
 		for(int j = 0; j < RESOLUTION; j++){
 			std::string color = std::to_string(plot[i][j].r) + " " + std::to_string(plot[i][j].g) + " " + std::to_string(plot[i][j].b) + " ";
@@ -52,11 +61,12 @@ void plotImage(Color plot[][RESOLUTION], std::ofstream &mandelbrot_file){
 		}
 		writeToFile("\n", mandelbrot_file);
 	}
+	mandelbrot_file.close();
 }
 
 Color calculatePixel(int px, int py){
-	double x0 = -2.5 + px * (3.5/RESOLUTION);
-	double y0 = -2 + py * (3.5/RESOLUTION);
+	double x0 = -2 + px * (2.5/RESOLUTION);
+	double y0 = -1.25 + py * (2.5/RESOLUTION);
 	double x = 0.0;
 	double y = 0.0;
 	int iteration = 0;
@@ -69,28 +79,26 @@ Color calculatePixel(int px, int py){
 	return getColor(iteration);
 }
 
-void mandelbrot(std::ofstream &mandelbrot_file){
-	Color plot[RESOLUTION][RESOLUTION];
+Color ** initPlot(){
+	Color ** plot = new Color*[RESOLUTION];
+	for(int i = 0; i < RESOLUTION; i++){
+		plot[i] = new Color[RESOLUTION];
+	}
+	return plot;
+}
+
+void mandelbrot(){
+	Color ** plot = initPlot();
 	for(int i = 0; i < RESOLUTION; i++){
 		for(int j = 0; j < RESOLUTION; j++){
 			plot[i][j] = calculatePixel(j, i);
 		}
 	}
-	plotImage(plot, mandelbrot_file);
-}
-
-std::ofstream setupFile(){
-	std::ofstream mandelbrot_file(OUTPUT_FILE);
-	mandelbrot_file << "P3" << std::endl;
-	mandelbrot_file << RESOLUTION << " " << RESOLUTION << std::endl;
-	mandelbrot_file << "255" << std::endl;
-	return mandelbrot_file;
+	plotImage(plot);
 }
 
 void masterRoutine(){
-	std::ofstream mandelbrot_file = setupFile();
-	mandelbrot(mandelbrot_file);
-	mandelbrot_file.close();
+	mandelbrot();
 }
 
 void slaveRoutine(){
