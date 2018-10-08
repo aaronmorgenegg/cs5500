21c408bbdb66242a4e38f74d23846c36f9aab55c
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-10-08 14:25:13 -0600

    Initial commit for hw 6

---
+++ assn6/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <unistd.h>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <vector>
+
+int main(int argc, char** argv) {
+	MPI_Init(&argc, &argv);
+	srand(time(NULL));
+	int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	MPI_Finalize();
+	return 0;
+}
+
