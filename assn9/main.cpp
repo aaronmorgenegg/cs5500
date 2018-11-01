272c1dd8390e0c1ba22d33a181b065a75976ecc1
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-11-01 12:49:56 -0600

    Fug bixes

--- assn9/main.cpp
+++ assn9/main.cpp
@@ -23,7 +23,7 @@ const int WORK_TO_GENERATE = 1024;
 const int VERBOSITY = 3;
 
 int getJob(){
-	return rand.randint()%MAX_WORK_SIZE+1;
+	return rand()%MAX_WORK_SIZE+1;
 }
 
 void initWork(int world_rank){
@@ -34,7 +34,7 @@ void initWork(int world_rank){
 	}
 }
 
-void receiveNewJobs(int new_job, int my_request, int job_flag, int my_status, int world_rank, std::vector<int> &job_queue){
+void receiveNewJobs(int new_job, MPI_Request my_request, int job_flag, MPI_Status my_status, int world_rank, std::vector<int> &job_queue){
 	MPI_Irecv(&new_job,1,MPI_INT,ANY,JOB,MCW,&my_request);
 	MPI_Test(&my_request,&job_flag,&my_status);
 	if(!job_flag) return;
@@ -58,8 +58,8 @@ void distributeWork(std::vector<int> &job_queue, int world_size, int world_rank)
 		job_queue.pop_back();
 		int job2 = job_queue.back();
 		job_queue.pop_back();
-		int dest1 = rand.randint()%world_size;
-		int dest2 = rand.randint()%world_size;
+		int dest1 = rand()%world_size;
+		int dest2 = rand()%world_size;
 
 		if(VERBOSITY>2) std::cout<<"p"<<world_rank<<": distributing job"<<job1<<" to p"<<dest1<<std::endl;
 		if(VERBOSITY>2) std::cout<<"p"<<world_rank<<": distributing job"<<job2<<" to p"<<dest2<<std::endl;
@@ -80,19 +80,19 @@ void loadBalance(int world_rank, int world_size){
 	int new_job;
 	int job;
 	int job_flag;
-	int jobs_to_spawn = WORK_TO_GENERATE + rand.randint()%WORK_TO_GENERATE;
+	int jobs_to_spawn = WORK_TO_GENERATE + rand()%WORK_TO_GENERATE;
 	int spawned_jobs = 0;
 	std::vector<int> job_queue;
 	MPI_Request my_request;
 	MPI_Status my_status;
 
 	while(1){
-		receiveNewJobs(new_job, my_request, job_flag, my_status, world_rank);
+		receiveNewJobs(new_job, my_request, job_flag, my_status, world_rank, job_queue);
 		distributeWork(job_queue, world_size, world_rank);
 		doWork(job_queue, world_rank);
 		generateNewWork(jobs_to_spawn, spawned_jobs, job_queue, world_rank);
 	}
-	if(VERBOSITY>0)std::cout<<"p"<<world_rank<<": finished"std::endl;
+	if(VERBOSITY>0)std::cout<<"p"<<world_rank<<": finished"<<std::endl;
 }
 
 int main(int argc, char** argv) {
