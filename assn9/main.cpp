f0ec5389a6248784637897348bf38a5ac9cf7eb8
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-11-01 12:41:16 -0600

    Added generate work function, added console output with verbosity

--- assn9/main.cpp
+++ assn9/main.cpp
@@ -14,16 +14,20 @@
 #define WHITE 1
 #define BLACK 2
 #define JOB 11
+#define TOKEN 12
 #define ANY MPI_ANY_SOURCE
 
 const int MAX_WORK_SIZE = 1024; // Note: sleep work is in ms
 const int MAX_JOB_QUEUE_SIZE = 16;
+const int WORK_TO_GENERATE = 1024;
+const int VERBOSITY = 3;
 
 int getJob(){
 	return rand.randint()%MAX_WORK_SIZE+1;
 }
 
 void initWork(int world_rank){
+	if(VERBOSITY>0)std::cout<<"p"<<world_rank<<": starting..."<<std::endl;
 	if(world_rank == 0){
 		int job = getJob();
 		MPI_Send(&job,1,MPI_INT,0,JOB,MCW);
@@ -35,11 +39,12 @@ void receiveNewJobs(int new_job, int my_request, int job_flag, int my_status, in
 	MPI_Test(&my_request,&job_flag,&my_status);
 	if(!job_flag) return;
 	job_queue.push_back(new_job);
-	std::cout<<"p"<<world_rank<<": received job "<<new_job<<std::endl;
+	if(VERBOSITY > 1) std::cout<<"p"<<world_rank<<": received job "<<new_job<<std::endl;
 }
 
-void doWork(std::vector<int> &job_queue){
+void doWork(std::vector<int> &job_queue, int world_rank){
 	if(job_queue.size() > 0){
+		if(VERBOSITY>2) std::cout<<"p"<<world_rank<<": doing job"<<job_queue[0]<<std::endl;
 		usleep(job_queue[0]);
 		job_queue.erase(job_queue.begin());
 	} else {
@@ -47,7 +52,7 @@ void doWork(std::vector<int> &job_queue){
 	}
 }
 
-void distributeWork(std::vector<int> &job_queue, int world_size){
+void distributeWork(std::vector<int> &job_queue, int world_size, int world_rank){
 	if(job_queue.size()>MAX_JOB_QUEUE_SIZE){
 		int job1 = job_queue.back();
 		job_queue.pop_back();
@@ -56,8 +61,18 @@ void distributeWork(std::vector<int> &job_queue, int world_size){
 		int dest1 = rand.randint()%world_size;
 		int dest2 = rand.randint()%world_size;
 
+		if(VERBOSITY>2) std::cout<<"p"<<world_rank<<": distributing job"<<job1<<" to p"<<dest1<<std::endl;
+		if(VERBOSITY>2) std::cout<<"p"<<world_rank<<": distributing job"<<job2<<" to p"<<dest2<<std::endl;
 		MPI_Send(&job1,1,MPI_INT,dest1,JOB,MCW);
 		MPI_Send(&job2,1,MPI_INT,dest2,JOB,MCW);
+	}
+}
+
+void generateNewWork(int jobs_to_spawn, int &spawned_jobs, std::vector<int> &job_queue, int world_rank){
+	if(spawned_jobs < jobs_to_spawn){
+		if(VERBOSITY>2) std::cout<<"p"<<world_rank<<": spawning additional job"<<std::endl;
+		spawned_jobs++;
+		job_queue.push_back(getJob());
 	}
 }
 
@@ -65,16 +80,19 @@ void loadBalance(int world_rank, int world_size){
 	int new_job;
 	int job;
 	int job_flag;
+	int jobs_to_spawn = WORK_TO_GENERATE + rand.randint()%WORK_TO_GENERATE;
+	int spawned_jobs = 0;
 	std::vector<int> job_queue;
 	MPI_Request my_request;
 	MPI_Status my_status;
 
 	while(1){
 		receiveNewJobs(new_job, my_request, job_flag, my_status, world_rank);
-		distributeWork(job_queue, world_size);
-		doWork(job_queue);
-		// generateNewWork();
+		distributeWork(job_queue, world_size, world_rank);
+		doWork(job_queue, world_rank);
+		generateNewWork(jobs_to_spawn, spawned_jobs, job_queue, world_rank);
 	}
+	if(VERBOSITY>0)std::cout<<"p"<<world_rank<<": finished"std::endl;
 }
 
 int main(int argc, char** argv) {
