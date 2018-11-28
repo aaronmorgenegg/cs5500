83323b632489fa3cb84dd11f913e4a806483d60a
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-11-27 18:35:32 -0700

    Fixing compiler errors

--- assn11/main.cpp
+++ assn11/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <time.h>
 #include <stdlib.h>
 #include <mpi.h>
@@ -12,11 +13,20 @@
 #define MCW MPI_COMM_WORLD
 #define ANY MPI_ANY_SOURCE
 
+
+using Clock = std::chrono::steady_clock;
+using std::chrono::time_point;
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+using namespace std::literals::chrono_literals;
+
 // ----- CONSTANTS -----
 
 std::string FILE_SHAKESPEARE = "sample_strings/shakespeare.txt"; // Complete works of shakespeare
 std::string FILE_DNA1 = "sample_strings/dna1.fasta"; // Dna sequence
 int BINARY_STRING_LENGTH = 10000000; // Length of binary strings for study
+int SHAKESPEARE_STRING_LENGTH = 100000; // Length of shakespeare string
+int DNA_STRING_LENGTH = 100000; // Length of DNA string
 int MIN_M = 1; // Smallest length of pattern for study
 int MAX_M = 100000; // Largest length of pattern for study
 int NUM_CHARS = 256; // number of characters in the alphabet used
@@ -87,13 +97,113 @@ std::string readFileToString(std::string filename){
 	return sstr.str();
 }
 
+// ----- ALGORITHMS -----
+
+std::vector<int> stringMatchingNaive(std::string text, std::string pattern){
+        // Naive string matching algorithm with O(n*m)
+        std::vector<int> found_indices;
+        int n = text.length();
+        int m = pattern.length();
+        for(int i = 0; i < n-m+1; i++){
+                for(int j = 0; j < m; j++){
+                        if(text[i+j] != pattern[j]){
+                                break;
+                        }
+                        if(j == m-1){
+                                found_indices.push_back(i);
+                        }
+                }
+        }
+        return found_indices;
+}
+
+std::vector<int> stringMatchingKMP(std::string text, std::string pattern){
+        // Knuth Morris Pratt string matching algorithm with O(n)
+        std::vector<int> found_indices;
+        int n = text.length();
+        int m = pattern.length();
+
+        // precompute prefix/suffic array
+        std::vector<int> lps = computeLPSArray(pattern, m);
+
+        int i = 0;
+        int j = 0;
+        while(i < n){
+                if(pattern[j]==text[i]){
+                        i++;
+                        j++;
+                }
+                if(j==m){
+                        found_indices.push_back(i-j);
+                        j = lps[j-1];
+                }
+                else if(i<n && pattern[j]!=text[i]){
+                        if(j!=0){
+                                j = lps[j-1];
+                        } else{
+                                i ++;
+                        }
+                }
+        }
+        return found_indices;
+}
+
+std::vector<int> stringMatchingBM(std::string text, std::string pattern){
+        // Boyer Moore string matching algorithm with O(nm) in the worst
+        // case, but O(n/m) in the best case
+        std::vector<int> found_indices;
+        int n = text.length();
+        int m = pattern.length();
+
+        std::vector<int> bad_char = badCharHeuristic(pattern, m);
+
+        int s = 0;
+        while(s<=n-m){
+                int j = m - 1;
+                while(j >= 0 && pattern[j] == text[s+j]){
+                        j--;
+                }
+                if(j<0){
+                        found_indices.push_back(s);
+                        if(s+m<n) s += (m-bad_char[(int)text[s+m]]);
+                        else s++;
+                } else{
+                        int value = (j-bad_char[(int)text[s+j]]);
+                        if(value>1) s += value;
+                        else s++;
+                }
+        }
+        return found_indices;
+}
+
 // ----- STUDY -----
 
-void runStudy(int world_rank, int world_size){
-	runBinaryRandomStudy(world_rank, world_size);
-	runBinaryRegularStudy(world_rank, world_size);
-	runShakespeareStudy(world_rank, world_size);
-	runDNAStudy(world_rank, world_size);
+void runAlgorithm(std::string text, int m, int world_rank, int world_size, int alg_code){
+        std::vector<double> study_data;
+        study_data.push_back((double)alg_code);
+        study_data.push_back((double)m);
+        time_point<Clock> start = Clock::now();
+        int seg_start = (text.length()/world_size)*world_rank;
+        int seg_length = (text.length()/world_size);
+        if(alg_code == NAIVE){
+                stringMatchingNaive(text.substr(seg_start, seg_length), text.substr(text.length()-m));
+        } else if(alg_code == KMP){
+                stringMatchingKMP(text.substr(seg_start, seg_length), text.substr(text.length()-m));
+        } else if(alg_code == BM){
+                stringMatchingBM(text.substr(seg_start, seg_length), text.substr(text.length()-m));
+        }
+        time_point<Clock> end = Clock::now();
+        milliseconds time = duration_cast<milliseconds>(end-start);
+        study_data.push_back((double)time.count());
+        MPI_Send(&study_data[0], 3, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+}
+
+void tallyResults(int world_rank, int world_size){
+        // If root, gather up timing data, sum and output it
+        // data is sent as an array of floats, [ALGORITHM_CODE, m, time]
+        // if not root, wait until root gives the go signal
+        // TODO
+
 }
 
 void runBinaryRandomStudy(int world_rank, int world_size){
@@ -117,7 +227,7 @@ void runBinaryRandomStudy(int world_rank, int world_size){
 	tallyResults(world_rank, world_size);
 }
 
-void runBinaryRegularyStudy(int world_rank, int world_size){
+void runBinaryRegularStudy(int world_rank, int world_size){
 	std::cout<<"-----Binary Regular Study-----"<<std::endl;
 	std::string binary_regular;
 	if(world_rank == 0){
@@ -164,7 +274,7 @@ void runDNAStudy(int world_rank, int world_size){
 	std::string dna;
 	if(world_rank == 0){
 		if(VERBOSITY>1) std::cout<<"Reading dna string from file..."<<std::endl;
-		dna = readFileToString(FILE_DNA);
+		dna = readFileToString(FILE_DNA1);
 		for(int i = 1; i < world_size; i++){
 			MPI_Send(dna.c_str(), dna.size(), MPI_CHAR, i, 0, MPI_COMM_WORLD);
 		}
@@ -180,111 +290,11 @@ void runDNAStudy(int world_rank, int world_size){
 	tallyResults(world_rank, world_size);
 }
 
-void runAlgorithm(std::string text, int m, int world_rank, int world_size, int alg_code){
-	std::vector<double> study_data;
-	study_data.push_back((double)alg_code);
-	study_data.push_back((double)m);
-	time_point<Clock> start = Clock::now();
-	int seg_start = (text.length()/world_size)*world_rank;
-	int seg_length = (text.length()/world_size);
-	if(alg_code == NAIVE){
-		stringMatchingNaive(text.substr(seg_start, seg_length), text.substr(text.length()-m));
-	} else if(alg_code == KMP){
-		stringMatchingKMP(text.substr(seg_start, seg_length), text.substr(text.length()-m));
-	} else if(alg_code == BM){
-		stringMatchingBM(text.substr(seg_start, seg_length), text.substr(text.length()-m));
-	}
-	time_point<Clock> end = Clock::now();
-	milliseconds time = duration_cast<milliseconds>(end-start);
-	study_data.push_back((double)diff.count());
-	MPI_Send(&study_data[0], 3, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-}
-
-void tallyResults(int world_rank, int world_size){
-	// If root, gather up timing data, sum and output it
-	// data is sent as an array of floats, [ALGORITHM_CODE, m, time]
-	// if not root, wait until root gives the go signal
-	// TODO
-	
-}
-
-// ----- ALGORITHMS -----
-
-std::vector<int> stringMatchingNaive(std::string text, std::string pattern){
-	// Naive string matching algorithm with O(n*m)
-	std::vector<int> found_indices;
-	int n = text.length();
-	int m = pattern.length();
-	for(int i = 0; i < n-m+1; i++){
-		for(int j = 0; j < m; j++){
-			if(text[i+j] != pattern[j]){
-				break;
-			}
-			if(j == m-1){
-				found_indices.push_back(i);
-			}
-		}
-	}
-	return found_indices;
-}
-
-std::vector<int> stringMatchingKMP(std::string text, std::string pattern){
-	// Knuth Morris Pratt string matching algorithm with O(n)
-	std::vector<int> found_indices;
-	int n = text.length();
-	int m = pattern.length();
-
-	// precompute prefix/suffic array
-	std::vector<int> lps = computeLPSArray(pattern, m);
-	
-	int i = 0;
-	int j = 0;
-	while(i < n){
-		if(pattern[j]==text[i]){
-			i++;
-			j++;
-		}
-		if(j==m){
-			found_indices.push_back(i-j);
-			j = lps[j-1];
-		}
-		else if(i<n && pattern[j]!=text[i]){
-			if(j!=0){
-				j = lps[j-1];
-			} else{
-				i ++;
-			}
-		}
-	}
-	return found_indices;
-}
-
-std::vector<int> stringMatchingBM(std::string text, std::string pattern){
-	// Boyer Moore string matching algorithm with O(nm) in the worst
-	// case, but O(n/m) in the best case
-	std::vector<int> found_indices;
-	int n = text.length();
-	int m = pattern.length();
-
-	std::vector<int> bad_char = badCharHeuristic(pattern, m);
-
-	int s = 0;
-	while(s<=n-m){
-		int j = m - 1;
-		while(j >= 0 && pattern[j] == text[s+j]){
-			j--;
-		}
-		if(j<0){
-			found_indices.push_back(s);
-			if(s+m<n) s += (m-bad_char[(int)text[s+m]]);
-			else s++;
-		} else{
-			int value = (j-bad_char[(int)text[s+j]]);
-			if(value>1) s += value;
-			else s++;
-		}
-	}
-	return found_indices;
+void runStudy(int world_rank, int world_size){
+        runBinaryRandomStudy(world_rank, world_size);
+        runBinaryRegularStudy(world_rank, world_size);
+        runShakespeareStudy(world_rank, world_size);
+        runDNAStudy(world_rank, world_size);
 }
 
 // ----- MAIN -----
@@ -296,20 +306,9 @@ int main(int argc, char** argv) {
 	MPI_Comm_size(MCW, &world_size);
 	int world_rank;
 	MPI_Comm_rank(MCW, &world_rank);
-	// MPI_Send(&sendData,1,MPI_INT,dest,0,MPI_COMM_WORLD);
-        // MPI_Recv(&recvData,1,MPI_INT,dest,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-	runStudy(world_rank, world_size)
-	std::string test_text = generateBinaryString(100, 0.5);
-	std::string test_pat = generateBinaryString(3, 0.5);
-	std::vector<int> sol = stringMatchingBM(test_text, test_pat);
-	std::cout <<test_text<<std::endl;
-	std::cout << test_pat<<std::endl;
-	std::cout <<"Indices found at:"<<std::endl;
-	for (auto i = sol.begin(); i != sol.end(); i++){
-		std::cout << *i << ' ';
-	}
-	std::cout << std::endl;
-
+	
+	runStudy(world_rank, world_size);
+	
 	MPI_Finalize();
 	return 0;
 }
